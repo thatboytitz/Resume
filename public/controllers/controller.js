18054e1089ce4fb210b914fc0e62eb5de4399281
thatboytitz <mmejia1337@gmail.com>
2016-11-29 21:39:59 -0800

    Items serverside
    request items from mongo this time

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,83 +1,77 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
-      edu1 = {
-          school : 'Cal State Los Angeles',
-          degree : 'Computer Science',
-          last_year : '2016'
-      };
+      console.log("Hello World from controller");
+      $http.get('/resume').success(function(response){
+           console.log("I got the data I requested");
+           $scope.resume = response;
+      });
 
-      edu2 = {
-          school : 'Dummy',
-          degree : 'Computer Science',
-          last_year : '9999'
-      };
+      // var eduList = [
+      //       {          
+      //             school : 'Cal State Los Angeles',
+      //             degree : 'Computer Science',
+      //             last_year : '2016'
+      //       },
+      //       {
+      //             school : 'Dummy',
+      //             degree : 'Computer Science',
+      //             last_year : '9999'
+      //       },
+      //       {
+      //             school : 'Dummy',
+      //             degree : 'Computer Science',
+      //             last_year : '9999'
+      //       },
+      //       {
+      //             school : 'Dummy',
+      //             degree : 'Computer Science',
+      //             last_year : '9999'
+      //       },
+      //       {
+      //             school : 'Dummy',
+      //             degree : 'Computer Science',
+      //             last_year : '9999'
+      //       }
+      // ];
 
-      var eduList = [
-            {          
-                  school : 'Cal State Los Angeles',
-                  degree : 'Computer Science',
-                  last_year : '2016'
-            },
-            {
-                  school : 'Dummy',
-                  degree : 'Computer Science',
-                  last_year : '9999'
-            },
-            {
-                  school : 'Dummy',
-                  degree : 'Computer Science',
-                  last_year : '9999'
-            },
-            {
-                  school : 'Dummy',
-                  degree : 'Computer Science',
-                  last_year : '9999'
-            },
-            {
-                  school : 'Dummy',
-                  degree : 'Computer Science',
-                  last_year : '9999'
-            }
-      ];
+      // var skillList = [
+      //       {skill : 'HTML'},
+      //       {skill : 'Bootstrap'},
+      //       {skill : 'Javascript'},
+      //       {skill : 'Angular JS'},
+      //       {skill : 'Node JS'},
+      //       {skill : 'Java'},
+      //       {skill : 'Many skills'}
+      // ];
 
-      var skillList = [
-            {skill : 'HTML'},
-            {skill : 'Bootstrap'},
-            {skill : 'Javascript'},
-            {skill : 'Angular JS'},
-            {skill : 'Node JS'},
-            {skill : 'Java'},
-            {skill : 'Many skills'}
-      ];
+      // var projectList = [
+      //       {
+      //             title : 'Project Title',
+      //             description : 'Project Description'
+      //       },
+      //       {
+      //             title : 'Resume',
+      //             description : 'This'
+      //       },
+      //       {
+      //             title : 'Project Title',
+      //             description : 'Project Description'
+      //       },
+      //       {
+      //             title : 'Project Title',
+      //             description : 'Project Description'
+      //       },
+      //       {
+      //             title : 'Project Title',
+      //             description : 'Project Description'
+      //       }
 
-      var projectList = [
-            {
-                  title : 'Project Title',
-                  description : 'Project Description'
-            },
-            {
-                  title : 'Resume',
-                  description : 'This'
-            },
-            {
-                  title : 'Project Title',
-                  description : 'Project Description'
-            },
-            {
-                  title : 'Project Title',
-                  description : 'Project Description'
-            },
-            {
-                  title : 'Project Title',
-                  description : 'Project Description'
-            }
+      // ];
 
-      ];
-
-      $scope.eduList = eduList;
-      $scope.skillList = skillList;
-      $scope.projectList = projectList;
+      // $scope.eduList = eduList;
+      // $scope.skillList = skillList;
+      // $scope.projectList = projectList;
+      // $scope.resume = resume;
 }]);﻿
 
 
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('contactlist', ['contactlist']);
-var bodyParser = require('body-parser');
+var db = mongojs('resume', ['resume']);
+// var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
+app.get('/resume', function (req, res) {
+      console.log('I received a GET request');
+
+
+      db.resume.find(function (err, docs) {
+            console.log(docs);
+            res.json(docs);
+      });
+});
 
 app.listen(8080);
 console.log("Server running on port 8080");
